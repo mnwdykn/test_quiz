a2b9e6625ec3d918ca4b08e037abac75fdd723a7
mnwdy <mnwdykn@gmail.com>
2025-04-26 19:18:50 +0900

    ホーム画面のレイアウトを改善

--- app/page.js
+++ app/page.js
@@ -2,10 +2,27 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2>/app/page.js</h2>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "100px",
+      }}
+    >
+      <h1>ITクイズアプリへようこそ！</h1>
+      <p>あなたのIT知識を試してみましょう！</p>
       <Link href="/quiz">
-        <button>/app/quiz/page.jsx</button>
+        <button
+          style={{
+            marginTop: "30px",
+            padding: "15px 30px",
+            fontSize: "18px",
+            cursor: "pointer",
+          }}
+        >
+          クイズを開始する
+        </button>
       </Link>
     </div>
   );
