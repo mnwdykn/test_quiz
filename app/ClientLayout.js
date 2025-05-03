d8c43dfcb65cb9ea92832a17f0f1bb260f80486c
mnwdy <mnwdykn@gmail.com>
2025-05-03 15:28:55 +0900

    レイアウト構成を分離：ClientLayoutとClientHeaderを整理

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -1,14 +1,7 @@
-// app/ClientLayout.jsx
+// ClientLayout.jsx
 "use client";
-
 import { SessionProvider } from "next-auth/react";
-import LoginButton from "../components/LoginButton"; // 共通表示のログイン状態
 
 export default function ClientLayout({ children }) {
-  return (
-    <SessionProvider>
-      <LoginButton />
-      {children}
-    </SessionProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 }
